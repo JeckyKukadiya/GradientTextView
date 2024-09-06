8c1d1053a11389838acc566fbdc6d1c3ba56f9d3
“Jecky” <jackykukadiya@gmail.com>
2024-09-06 16:36:44 +0530

    Initial Commit

---
+++ Sources/GradientTextView/TextView.swift
@@ -0,0 +1,39 @@
+//
+//  TextView.swift
+//  GradientText
+//
+//  Created by Eryus Tech on 06/09/24.
+//
+
+import SwiftUI
+
+struct GradientTextView: View {
+    var text: LocalizedStringKey
+    var gradientColors: [Color] = [Color.red, Color.green, Color.blue]
+    var size: CGFloat = 24
+    var font: Font? = nil
+    
+    var body: some View {
+        Text(text)
+            .foregroundColor(.clear)
+            .background(LinearGradient(gradient: Gradient(colors: gradientColors), startPoint: .leading, endPoint: .trailing))
+            .mask(Text(text))
+            .font(font ?? Font.custom("Poppins-Bold", size: size))
+    }
+}
+
+extension GradientTextView {
+    init(
+        text: String = "",
+        gradientColors: [Color] = [Color.red, Color.green, Color.blue],
+        size: CGFloat = 24,
+        font: Font? = nil
+    ) {
+        self.init(text: LocalizedStringKey(text), gradientColors: gradientColors, size: size, font: font)
+    }
+    
+}
+
+#Preview {
+    GradientTextView(text: "Hello, Louise!", gradientColors: [Color.yellow, Color.purple, Color.black])
+}
